0931501bed7f87042f75c92fbbbb0e454e8f37cd
Shahed <shahadalali7@gmail.com>
2021-01-17 09:28:35 -0800

    Add new folder (Inheritance)
    Add Inheritance folder and SaleSystem
    (SaleSytem is not tracked )

---
+++ Inheritance/SaleSystem/Sale.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shahed.classes;
+
+/**
+ *
+ * @author DELL
+ */
+public class Sale {
+
+    private double amount;
+
+    public Sale() {
+        amount = 0.0;
+    }
+
+    public Sale(double amount) {
+        this.amount = amount;
+    }
+
+    public Sale(Sale saleObject) {
+        this.amount = saleObject.amount;
+
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+
+    }
+
+    public void saleDetails() {
+        System.out.println("The sale amount is " + amount);
+    }
+
+    public boolean equals(Object otherObject) {
+        if (otherObject == null) {
+            return false;
+        }
+        if (getClass() != otherObject.getClass()) {
+            return false;
+        }
+        Sale sale = (Sale) otherObject;
+        return (amount == sale.amount);
+    }
+
+}
