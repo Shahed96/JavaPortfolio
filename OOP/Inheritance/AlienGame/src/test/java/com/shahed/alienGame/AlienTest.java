065465c79740b3a8c2ce2bfda08e807c4dda75ad
Shahed <shahadalali7@gmail.com>
2021-01-19 07:09:18 -0800

    Update
    Add @BeforeEach to create Alien object before each method instead of creating it inside each method

--- OOP/Inheritance/AlienGame/src/test/java/com/shahed/alienGame/AlienTest.java
+++ OOP/Inheritance/AlienGame/src/test/java/com/shahed/alienGame/AlienTest.java
@@ -1,23 +1,30 @@
 package com.shahed.alienGame;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-
 
 /**
  * Unit test for Alien class.
  */
-public class AlienTest 
-{
-    
-    @Test
-    public void getHealthTest()
+public class AlienTest {
+
+    Alien alien;
+
+    @BeforeEach
+    void init() //create an instance for each method
     {
-        //create an object
-        Alien alien =new MarshmallowManAlien(99,"Marshmallow");
-        
+        alien = new MarshmallowManAlien(99, "Marshmallow");
+    }
+    @Disabled 
+    @Test
+    public void testGetHealth() {
+
         //compare the expected with the real value
-       assertEquals(99, alien.getHealth());
+        assertEquals(99, alien.getHealth(),
+                "The method should return the health of the alien (int value)");
+        assertNotNull(alien); //to check the object is not null
+
     }
 }
