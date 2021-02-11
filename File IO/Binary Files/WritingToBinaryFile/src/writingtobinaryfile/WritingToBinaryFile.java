3fe961e2bfb5f4d3e1e64d1cfdf99989efc34ab7
Shahed <shahadalali7@gmail.com>
2021-02-11 11:00:40 -0800

    commit

---
+++ File IO/Binary Files/WritingToBinaryFile/src/writingtobinaryfile/WritingToBinaryFile.java
@@ -0,0 +1,24 @@
+
+package writingtobinaryfile;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**
+ *
+ * @author Shahed A.
+ */
+public class WritingToBinaryFile {
+
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+       ObjectOutputStream outputStream =new ObjectOutputStream(new FileOutputStream("numbers.dat"));
+       for (int i=0; i<5; i++)
+       {
+       outputStream.writeInt(i);
+       }
+    }
+    
+}
