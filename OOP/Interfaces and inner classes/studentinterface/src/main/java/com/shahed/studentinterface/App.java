ae53e321e981df08bfdccace4931e1d50864d0ce
Shahed <shahadalali7@gmail.com>
2021-02-13 05:38:37 -0800

    upload
    upload

--- OOP/Interfaces and inner classes/studentinterface/src/main/java/com/shahed/studentinterface/App.java
+++ OOP/Interfaces and inner classes/studentinterface/src/main/java/com/shahed/studentinterface/App.java
@@ -1,13 +1,63 @@
 package com.shahed.studentinterface;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //create an array of student
+       Student [] students =new Student[5];
+       
+       //construct each object
+       
+       students[0]=new Student("Reema",7777);
+       students[1]=new Student("Maram",55555);
+       students[2]=new Student("Kareem",99999);
+       students[3]=new Student("Nuha",99);
+       students[4]=new Student("Hanadi",99999);
+       
+       
+       sort(students);
+       
+       
+       
+    }
+    
+    public static void sort(Student[] a) {
+
+        int size = a.length;
+        int min, i;
+        for (i = 0; i < size - 1; i++) {
+            min = i;
+
+            for (int j = i + 1; j < size; j++) {
+                if (a[j].compareTo(a[min]) < 0) {
+                    min = j;
+                    swap(i, min, a);
+                }
+
+            }
+
+        }
+
+        display(a, size);
+
+    }
+
+    public static void swap(int i, int j, Student[] a) {
+        //swap function with temp variable:
+
+        Student temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+
+    }
+
+    public static void display(Student[] a, int size) {
+System.out.println("Students objects after sorting: ");
+        for (int i = 0; i < size; i++) {
+            System.out.println("Student's last name: "+a[i].getLastName()+" stdent's ID "+a[i].getID());
+
+        }
     }
 }
--- OOP/Interfaces and inner classes/studentinterface/src/main/java/com/shahed/studentinterface/Student.java
+++ OOP/Interfaces and inner classes/studentinterface/src/main/java/com/shahed/studentinterface/Student.java
@@ -1,14 +1,62 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 package com.shahed.studentinterface;
 
 /**
  *
- * @author DELL
+ * @author Shahed A.
  */
-public class Student {
-    
+public class Student implements Comparable {
+
+    private String lastName;
+    private int ID;
+
+    public Student() {
+        lastName = "Not yet!";
+        ID = 00000000;
+    }
+
+    public Student(String lastName, int ID) {
+        this.lastName = lastName;
+        this.ID = ID;
+    }
+
+    public Student(Student otherStudent) {
+        this.lastName = otherStudent.lastName;
+        this.ID = otherStudent.ID;
+    }
+
+   
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void ID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public int compareTo(Object otherObject) {
+
+        //convert it to Student object
+        Student studentObject = (Student) otherObject;
+        // comparasion using ID:
+        if (studentObject.ID == this.ID) {
+            return 0;
+        } else if (studentObject.ID > this.ID) {
+            return -1;
+        }
+        return 1;
+    }
+
 }
