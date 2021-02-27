2ff4c108b1ca012fe5469667bfc03a5cc2f6b74e
Shahed <shahadalali7@gmail.com>
2021-02-27 10:06:45 -0800

    commit

--- Swing/other sources/Form with db/src/form/Form.java
+++ Swing/other sources/Form with db/src/form/Form.java
@@ -6,7 +6,12 @@ public class Form {
 
     
     public static void main(String[] args) {
-       
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginForm().setVisible(true);
+            }
+        });
+  
     }
     
 }
