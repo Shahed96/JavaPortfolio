4a7848d4c7a1cdba31bbd3b460dfb390eb07a7a1
Shahed <shahadalali7@gmail.com>
2021-06-12 16:29:00 -0700

    selection Sort

---
+++ Data Structure/Sorting algorithms/Simple Sort/Selection sort/src/selection/sort/SelectionSort.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package selection.sort;
+
+/**
+ *
+ * @author Shahed A. Abdulwahhb
+ */
+public class SelectionSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+        int [] arr ={7,6,5,4,3,2,1,0};
+        selectionSort(arr);
+       
+    }
+
+    public static void selectionSort(int[] arr) {
+        int min;
+        int size = arr.length;
+        for (int i = 0; i < size - 1; i++) {
+            min = i;
+            for (int j = i+1; j < size; j++) {
+
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+                swap(i, min, arr);
+            }
+
+        }
+        dispalyTheSortedArray(arr);
+
+    }
+
+    private static void swap(int i, int j, int[] a) {
+        //swap function with temp variable:
+
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+
+    }
+
+    private static void dispalyTheSortedArray(int[] arr) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    System.out.println("The sorted Array:");
+        for (int i=0; i<arr.length; i++){
+    System.out.println(arr[i]);
+    
+    }
+    
+    }
+
+}
