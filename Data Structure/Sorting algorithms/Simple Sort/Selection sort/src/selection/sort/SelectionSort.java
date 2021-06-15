f39ff156edfc31b3b13f9bbf751c10aa48dd36c7
Shahed <shahadalali7@gmail.com>
2021-06-15 14:48:24 -0700

    com

--- Data Structure/Sorting algorithms/Simple Sort/Selection sort/src/selection/sort/SelectionSort.java
+++ Data Structure/Sorting algorithms/Simple Sort/Selection sort/src/selection/sort/SelectionSort.java
@@ -1,4 +1,3 @@
-
 package selection.sort;
 
 /**
@@ -12,19 +11,20 @@ public class SelectionSort {
      */
     public static void main(String[] args) {
 
-        int[] arr = {7, 6, 5, 4, 3, 2, 1, 0};
+        Integer[] arr = {7, 6, 5, 4, 3, 2, 1, 0};
         selectionSort(arr);
 
     }
+    //Generic method:
 
-    public static void selectionSort(int[] arr) {
+    public static <T extends Comparable<? super T>> void selectionSort(T[] arr) { //extends expressin to use compareTo method
         int min;
         int size = arr.length;
         for (int i = 0; i < size - 1; i++) {
             min = i;
             for (int j = i + 1; j < size; j++) {
 
-                if (arr[min] > arr[j]) {
+                if (arr[min].compareTo(arr[j]) > 0) {
                     min = j;
                 }
                 swap(i, min, arr);
@@ -35,16 +35,16 @@ public class SelectionSort {
 
     }
 
-    private static void swap(int i, int j, int[] a) {
+    private static <T> void swap(int i, int j, T[] a) {
         //swap function with temp variable:
 
-        int temp = a[i];
+        T temp = a[i];
         a[i] = a[j];
         a[j] = temp;
 
     }
 
-    private static void dispalyTheSortedArray(int[] arr) {
+    private static <T> void dispalyTheSortedArray(T[] arr) {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         System.out.println("The sorted Array:");
         for (int i = 0; i < arr.length; i++) {
